e1a472e04419aa8c85c37042b291d747eb2147a3
sasubramanian <sai.kathiresan@gmail.com>
2021-07-20 16:46:11 -0500

    ArgoCD module init

---
+++ main.tf
@@ -0,0 +1,23 @@
+provider "kubernetes" {
+  load_config_file       = false
+  cluster_ca_certificate = base64decode(var.kubernetes_cluster_cert_data)
+  host                   = var.kubernetes_cluster_endpoint
+  exec {
+    api_version = "client.authentication.k8s.io/v1alpha1"
+    command     = "aws-iam-authenticator"
+    args        = ["token", "-i", "${var.kubernetes_cluster_name}"]
+  }
+}
+
+provider "helm" {
+  kubernetes {
+    load_config_file       = false
+    cluster_ca_certificate = base64decode(var.kubernetes_cluster_cert_data)
+    host                   = var.kubernetes_cluster_endpoint
+    exec {
+      api_version = "client.authentication.k8s.io/v1alpha1"
+      command     = "aws-iam-authenticator"
+      args        = ["token", "-i", "${var.kubernetes_cluster_name}"]
+    }
+  }
+}
